eb9d948455ff20179904d196dd718b9d1ed7dfae
youngwoos <stats7445@gmail.com>
2020-03-20 11:43:17 +0900

    UTF-8

--- R/getdata.R
+++ R/getdata.R
@@ -9,17 +9,21 @@ getdata <- function(data) {
   # 01_patient ----------------------------------------------------------------
   if (data == "patient") {
     patient <-
-      utils::read.csv (
+      utils::read.csv(
         "https://github.com/jihoo-kim/Data-Science-for-COVID-19/raw/master/dataset/Patient/PatientInfo.csv",
         stringsAsFactors = F,
-        na.strings = ""
+        na.strings = "",
+        fileEncoding = "UTF-8"
       )
 
     # modify column type
     patient <- patient %>%
       tibble::as_tibble() %>%
-      dplyr::mutate_at(c("confirmed_date", "released_date", "deceased_date"),
-                lubridate::as_date)
+      dplyr::mutate_at(c("symptom_onset_date",
+                         "confirmed_date",
+                         "released_date",
+                         "deceased_date"),
+                       lubridate::as_date)
 
     return(patient)
   }
@@ -31,7 +35,8 @@ getdata <- function(data) {
       utils::read.csv (
         "https://github.com/jihoo-kim/Data-Science-for-COVID-19/raw/master/dataset/Patient/PatientRoute.csv",
         stringsAsFactors = F,
-        na.strings = ""
+        na.strings = "",
+        fileEncoding = "UTF-8"
       )
 
     # modify colum type
@@ -49,8 +54,9 @@ getdata <- function(data) {
       utils::read.csv (
         "https://github.com/jihoo-kim/Data-Science-for-COVID-19/raw/master/dataset/Time/Time.csv",
         stringsAsFactors = F,
-        na.strings = ""
-      )
+        na.strings = "",
+        fileEncoding = "UTF-8"
+        )
 
     # modify column type
     time <- time %>%
